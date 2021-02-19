e19d8f74d8f2b346c7f60435eecaadaa964d57d3
unknown <nellysherova@mail.ru>
2021-02-19 15:09:44 +0300

    Создала класс Matrix

--- MatrixHomeWork/Program.cs
+++ MatrixHomeWork/Program.cs
@@ -1,7 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace MatrixHomeWork
 {
+    class Matrix
+    {
+        public int Width {get; set;} // ширина
+        public int Height {get; set;} // высота
+        public int Lines {get; set;} // линии
+        public int StartDelay {get; set;} // остановка 
+        Matrix(){}
+        Matrix(int Width, int Height; int Lines; int StartDelay)
+        {
+            this.Width = Width;
+            this.Height = Height;
+            this.Lines = Lines;
+            this.StartDelay = StartDelay;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
