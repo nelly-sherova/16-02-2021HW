920ed9271ef5ba5e7881dd4f79c4f4600bceff9c
unknown <nellysherova@mail.ru>
2021-02-19 14:36:05 +0300

    Начало работы

---
+++ MatrixHomeWork/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MatrixHomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
